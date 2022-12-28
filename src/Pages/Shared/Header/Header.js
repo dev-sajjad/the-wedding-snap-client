863bd36f04437eb787a2fa81ea9f0970ed7ad838
dev-sajjad <sajjadbmarpc@gmail.com>
2022-12-28 21:50:39 +0600

    basic setup with routes and header, footer component created

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h2>Header area</h2>
+        </div>
+    );
+};
+
+export default Header;<h2>Header area</h2>
